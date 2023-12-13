2c53fe1003dd806423d057c24be7109b589b1b9c
WatermelonGalaxy <mccanna@wit.edu>
2023-12-13 04:26:44 -0500

    remove proxy url

--- balls-api.js
+++ balls-api.js
@@ -3,7 +3,7 @@
 async function getCurrentStandings() {
   //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const targetUrl = "https://api-web.nhle.com/v1/standings/now";
-  const response = await fetch(proxyUrl + targetUrl);
+  const response = await fetch(targetUrl);
   console.log(response);
   const data = await response.json();
 
@@ -105,7 +105,7 @@ async function getCurrentStandings() {
 async function getCurrentStandingsObj() {
   //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const targetUrl = "https://api-web.nhle.com/v1/standings/now";
-  const response = await fetch(proxyUrl + targetUrl);
+  const response = await fetch(targetUrl);
   console.log(response);
   const data = await response.json();
 
@@ -116,7 +116,7 @@ async function getCurrentStandingsObj() {
 async function getPlayers(teamAbbreviation) {
   //const proxyUrl = "https://cors-anywhere.herokuapp.com/";
   const targetUrl = `https://api-web.nhle.com/v1/roster/${teamAbbreviation}/current`;
-  const response = await fetch(proxyUrl + targetUrl);
+  const response = await fetch(targetUrl);
   console.log(response);
   const data = await response.json();
 
